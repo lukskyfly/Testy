3c84e0af578117667395b16a4bb3cb3990d66410
lukas <lukasz.grazda@gmail.com>
2020-09-14 14:58:43 +0200

    add Tests addKwh

--- testyMiernikaPradu/src/test/java/elektricity/ElectricityMeterTest.java
+++ testyMiernikaPradu/src/test/java/elektricity/ElectricityMeterTest.java
@@ -6,11 +6,40 @@ import org.junit.Test;
 public class ElectricityMeterTest {
 
     @Test
-    public void addKwh() {
-       ElectricityMeter electricityMeter = new ElectricityMeter();
-       electricityMeter.addKwh(1);
-       Assert.assertTrue(electricityMeter.getKwh()== 1
-       );
-
+    public void addKwh_newMeter_proprAddition() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 1);
     }
+    @Test
+    public void addKwh_newMeter2_proprAddition() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(4);
+        Assert.assertTrue(electricityMeter.getKwh() == 5);
+    }
+    @Test
+    public void addKwh_newMeter5_proprAddition() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(4);
+        Assert.assertTrue(electricityMeter.getKwh() == 17);
+    }
+    @Test
+    public void addKwhCounterIncreasIfNew() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 1);
+    }
+    @Test
+    public void addKwhCounterIncreasIfSecond() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 2);
+    }
+
 }
