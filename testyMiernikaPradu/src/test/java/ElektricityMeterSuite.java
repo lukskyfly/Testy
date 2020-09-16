e04502724a12fa0b5fa0822fa3aba8abb22bc0c3
lukas <lukasz.grazda@gmail.com>
2020-09-16 15:23:36 +0200

    add @RunWith,@Suite

---
+++ testyMiernikaPradu/src/test/java/ElektricityMeterSuite.java
@@ -0,0 +1,12 @@
+import elektricity.ElectricityMeterTest;
+import elektricity.TwoElectricityMetersTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ElectricityMeterTest.class,
+        TwoElectricityMetersTest.class
+})
+public class ElektricityMeterSuite {
+}
--- testyMiernikaPradu/src/test/java/elektricity/TwoElectricityMetersTest.java
+++ testyMiernikaPradu/src/test/java/elektricity/TwoElectricityMetersTest.java
@@ -4,56 +4,68 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class ElectricityMeterTest {
-
+public class TwoElectricityMetersTest {
     @Test
     public void addKwh_newMeter_proprAddition() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1 = new ElectricityMeter();
         electricityMeter.addKwh(1);
         Assert.assertTrue(electricityMeter.getKwh() == 1);
     }
+
     @Test
     public void addKwh_newMeter2_proprAddition() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1 = new ElectricityMeter();
         electricityMeter.addKwh(1);
         electricityMeter.addKwh(4);
         Assert.assertTrue(electricityMeter.getKwh() == 5);
     }
+
     @Test
     public void addKwh_newMeter5_proprAddition() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1 = new ElectricityMeter();
         electricityMeter.addKwh(1);
         electricityMeter.addKwh(4);
         electricityMeter.addKwh(4);
         electricityMeter.addKwh(4);
         electricityMeter.addKwh(4);
-        Assert.assertTrue("",electricityMeter.getKwh() == 17);
+        Assert.assertTrue("", electricityMeter.getKwh() == 17);
     }
+
     @Test
     public void addKwhCounterIncreasIfNew() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1 = new ElectricityMeter();
         electricityMeter.addKwh(1);
         Assert.assertTrue(electricityMeter.getKwh() == 1);
     }
+
     @Test
     public void addKwhCounterIncreasIfSecond() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1 = new ElectricityMeter();
         electricityMeter.addKwh(1);
         electricityMeter.addKwh(1);
         Assert.assertTrue(electricityMeter.getKwh() == 2);
     }
-    public void givenNewMeterWhenFirstAdditionThenProperCounter(){
+
+    public void givenNewMeterWhenFirstAdditionThenProperCounter() {
         //Given
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1= new ElectricityMeter();
         //When
         electricityMeter.addKwh(2);
         //Then
-        Assert.assertTrue(electricityMeter.getKwh()== 2);
+        Assert.assertTrue(electricityMeter.getKwh() == 2);
     }
+
     @Ignore("not implemented yet")
     @Test(expected = ArithmeticException.class)
     public void getHowMoreExpensiveNormalIs() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        ElectricityMeter electricityMeter1 = new ElectricityMeter();
         electricityMeter.setCentsForKwh(90);
         electricityMeter.getHowMoreExpensiveNormalIs();
     }
